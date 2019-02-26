f47d2f928be81436112824d4000dfbcebc50b8df
Pedro S <pedrocarlos.ti@gmail.com>
2019-02-25 21:18:34 -0300

    add: Navigation and Pages

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
-import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import './config/ReactotronConfig';
+import React from 'react';
+import Routes from './routes';
 
-import Welcome from '~/components/Welcome';
+const App = () => <Routes />;
 
-const bgColor = '#F5FCFF';
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: bgColor,
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
-
-export default class App extends Component {
-  state = {
-    message: 'Welcome to React Native!',
-  };
-
-  render() {
-    const { message } = this.state;
-
-    return (
-      <View style={styles.container}>
-        <Welcome title={message} />
-      </View>
-    );
-  }
-}
+export default App;
---
+++ src/pages/Issues/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+// import styles from './styles';
+
+const Issues = () => (
+  <View>
+    <Text>Issues</Text>
+  </View>
+);
+
+export default Issues;
